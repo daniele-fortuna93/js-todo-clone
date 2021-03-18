baffd59c60b5704a12fbb62ec1de722fc2afad9a
Daniele <bambudio@libero.it>
2021-03-18 16:28:10 +0100

    Aggiunta funzione clonaAggiungi

--- js/script.js
+++ js/script.js
@@ -6,16 +6,21 @@ var todoThings = [
   "Doccia",
   "Preparare pranzo"
 ];
+var cloneList = $('.template li'); // Elemento da clonare
+var todoList = $('.todo_list'); // Elemento a cui aggiungere l'elemento clonato
 
 // 2. Inserimento cose da fare
 for (var i = 0; i < todoThings.length; i++) {
-  var cloneList = $('.template li').clone();
-  cloneList.prepend(todoThings[i]);
-  $('.todo_list').append(cloneList);
+  clonaAggiungi(cloneList.clone(),todoThings[i],todoList);
 }
+// for (var i = 0; i < todoThings.length; i++) {
+//   var cloneList = $('.template li').clone(); // Clono l'elemento che dovrò inserire nella pagina
+//   cloneList.prepend(todoThings[i]);  // Aggiungo i valori all'elemento clonato
+//   $('.todo_list').append(cloneList); // Inserisco l'elemento clonato nella pagina
+// }
 
 // 3. Evento al click
-$('.todo_list').on('click','.todo_list-delete',
+$('.todo_list').on('click','.todo_list-delete', // Sempre in ascolto anche per elementi aggiunti dopo il caricamento della pagina
 function(){
   $(this).parent().remove();
 });
@@ -23,19 +28,27 @@ function(){
 $('#add_list').keydown(function (event) {
 
   if ( event.which == 13 ) {
-    var addThing = $(this).val();
-    if ( addThing != "" ) {
-      addThing = addThing[0].toUpperCase() + addThing.substring(1);
-      var cloneList = $('.template li').clone();
-
-      cloneList.prepend(addThing);
-
-      $('.todo_list').append(cloneList);
-
-      $(this).val("");
+    var addThing = $(this).val(); // Prendo il contenuto dell'input text
+    if ( addThing != "" ) { // Controllo che non sia vuoto
+      addThing = addThing[0].toUpperCase() + addThing.substring(1); // Trasformo la prima letterea in maiuscolo
+      // var cloneList = $('.template li').clone(); // Clono l'elemento che dovrò inserire nella pagina
+      // cloneList.prepend(addThing); // Aggiungo i valori all'elemento clonato
+      // $('.todo_list').append(cloneList); // Inserisco l'elemento clonato nella pagina
+      clonaAggiungi(cloneList.clone(),addThing,todoList);
+      $(this).val(""); // Pulisco l'input text
 
     }
-
-
   }
-})
+});
+
+// Funzioni
+// Funzione al quale passo 3 argomenti:
+// Il primo è l'elemento clonato.
+// Il secondo è l'elemento da aggiungere all'elemento clonato.
+// Il terzo è l'elemento a cui andrà aggiunto l'elemento clonato
+
+function clonaAggiungi(elementoClonato, elementoDaAggiungere, elementoPadre) {
+  var clonato = elementoClonato;
+  clonato.prepend(elementoDaAggiungere);
+  elementoPadre.append(clonato);
+}
